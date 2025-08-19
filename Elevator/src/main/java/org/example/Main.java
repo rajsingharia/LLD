8a5f404345883b536db702c3125da34b0db9080e
raj <rajsingharia.1234@gmail.com>
2025-08-19 22:12:36 +0530

    converting elevator service into differnet runnables

--- Elevator/src/main/java/org/example/Main.java
+++ Elevator/src/main/java/org/example/Main.java
@@ -11,6 +11,10 @@ package org.example;
 // - emergencyButton (diff strategy)
 
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 //DATA
 class CallLiftData {
@@ -19,15 +23,6 @@ class CallLiftData {
     CallLiftData(Integer floorCalledFrom, Direction floorCalledDirection) {
         this.floorCalledFrom = floorCalledFrom;
         this.floorCalledDirection = floorCalledDirection;
-    }
-}
-
-class NextLiftMove {
-    Integer floor;
-    Direction direction;
-    NextLiftMove(Integer floor, Direction direction) {
-        this.floor = floor;
-        this.direction = direction;
     }
 }
 
@@ -46,172 +41,70 @@ enum ElevatorState {
 
 //STRATEGY
 interface ElevatorMovementStrategy {
-    NextLiftMove findNextMove(ElevatorService elevatorService);
-}
 
-class OneDirectionElevatorMovementStrategy implements ElevatorMovementStrategy {
-
-    @Override
-    public NextLiftMove findNextMove(ElevatorService elevatorService) {
-
-        int currentFloor = elevatorService.getCurrentFloor();
-        Direction currentDirection = elevatorService.getDirection();
-
-        if(currentDirection == Direction.DOWN) {
-            currentFloor--;
-        } else {
-            currentFloor++;
-        }
-
-        if(currentFloor == elevatorService.getMinFloor()) {
-            currentDirection = Direction.UP;
-        } else if(currentFloor == elevatorService.getMaxFloor()) {
-            currentDirection = Direction.DOWN;
-        }
-
-        return new NextLiftMove(currentFloor, currentDirection);
-    }
+    CallLiftData findNextMove(List<CallLiftData> callLiftDataList, Integer currentFloor, Direction direction);
 }
 
 class NearestRequestStrategy implements ElevatorMovementStrategy {
+
     @Override
-    public NextLiftMove findNextMove(ElevatorService elevator) {
-        List<Integer> requests = new ArrayList<>(elevator.getGoToDataList());
-        elevator.getCallLiftDataList().forEach(c -> requests.add(c.floorCalledFrom));
-
-        if (requests.isEmpty()) return null;
-
-        // pick the nearest request floor
-        int curr = elevator.getCurrentFloor();
-        int next = requests.stream()
-                .min(Comparator.comparingInt(f -> Math.abs(f - curr)))
-                .get();
-
-        Direction dir = next > curr ? Direction.UP : Direction.DOWN;
-        return new NextLiftMove(next, dir);
+    public CallLiftData findNextMove(List<CallLiftData> callLiftDataList, Integer currentFloor, Direction direction) {
+        int minDifferent = Integer.MAX_VALUE;
+        CallLiftData nextLiftData = null;
+        for (CallLiftData callLiftData : callLiftDataList) {
+            int currDifference = Math.abs(currentFloor - callLiftData.floorCalledFrom);
+            if (currDifference < minDifferent) {
+                minDifferent = (currDifference);
+                nextLiftData = (callLiftData);
+            }
+        }
+        return nextLiftData;
     }
 }
 
 //ELEVATOR SERVICE
-class ElevatorService {
+class ElevatorService implements Runnable {
+    private final Integer elevatorId;
     private Integer currentFloor;
     private Direction direction;
-    private final Integer minFloor;
-    private final Integer maxFloor;
     private final List<CallLiftData> callLiftDataList;
-    private final List<Integer> goToDataList;
     private Gate gate;
     private final ElevatorMovementStrategy elevatorMovementStrategy;
     private ElevatorState elevatorState;
+    private boolean isRunning;
 
-    ElevatorService(Integer maxFloor, Integer minFloor, ElevatorMovementStrategy elevatorMovementStrategy) {
+    ElevatorService(int id, ElevatorMovementStrategy elevatorMovementStrategy) {
+        this.elevatorId = id;
+        this.isRunning = true;
         this.currentFloor = 0;
         this.direction = Direction.UP;
-        this.minFloor = minFloor;
-        this.maxFloor = maxFloor;
         this.callLiftDataList = new ArrayList<>();
-        this.goToDataList = new ArrayList<>();
         this.gate = Gate.CLOSE;
         this.elevatorMovementStrategy = elevatorMovementStrategy;
         this.elevatorState = ElevatorState.IDLE;
     }
 
-    public int getCurrentFloor() {
-        return currentFloor;
+    public Integer getElevatorId() {
+        return elevatorId;
     }
 
-    public void setCurrentFloor(Integer currentFloor) {
-        this.currentFloor = currentFloor;
-    }
-
-    public Direction getDirection() {
-        return direction;
-    }
-
-    public void setDirection(Direction direction) {
-        this.direction = direction;
-    }
-
-    public int getMaxFloor() {
-        return maxFloor;
-    }
-
-    public Integer getMinFloor() {
-        return minFloor;
-    }
-
-    public List<CallLiftData> getCallLiftDataList() {
-        return callLiftDataList;
-    }
-
-    public List<Integer> getGoToDataList() {
-        return goToDataList;
-    }
-
-    private void startMoving() {
-        if (this.elevatorState == ElevatorState.MOVING) {
-            // If already moving don't move again
-            return;
-        }
-        while(!getCallLiftDataList().isEmpty() || !getGoToDataList().isEmpty()) {
-            if(this.elevatorState == ElevatorState.DOOR_OPEN) {
-                continue;
+    private void goToFloor(Integer targetFloor) {
+        this.elevatorState = ElevatorState.MOVING;
+        while(!Objects.equals(this.currentFloor, targetFloor)) {
+            if(this.currentFloor < targetFloor) {
+                this.direction = Direction.UP;
+                this.currentFloor++;
+            } else {
+                this.direction = Direction.DOWN;
+                this.currentFloor--;
             }
-            this.elevatorState = ElevatorState.MOVING;
-            // get next move
-            NextLiftMove nextLiftMove = elevatorMovementStrategy.findNextMove(this);
-
-            setDirection(nextLiftMove.direction);
-            setCurrentFloor(nextLiftMove.floor);
-
-            // check if this is right to open for user in or out
-            checkIfGateCanOpen();
         }
         this.elevatorState = ElevatorState.IDLE;
     }
 
-    public void callLift(Integer floorCalledFrom, Direction fllorCalledDirection) {
+    public void requestElevator(Integer floorCalledFrom, Direction fllorCalledDirection) {
+        // Validate request
         callLiftDataList.add(new CallLiftData(floorCalledFrom, fllorCalledDirection));
-        startMoving();
-    }
-
-    public void goToFloor(Integer floorToGoTo) {
-        goToDataList.add(floorToGoTo);
-    }
-
-    private Optional<CallLiftData> isCallLiftIsInSameFloorAndDirection(Integer currentFloor, Direction currentDirection) {
-        return callLiftDataList
-                .stream()
-                .filter(callLift -> Objects.equals(callLift.floorCalledFrom, currentFloor) && callLift.floorCalledDirection == currentDirection)
-                .findFirst();
-    }
-
-    private Optional<Integer> isGoToIsInSameFloor(Integer currentFloor) {
-        return goToDataList
-                .stream()
-                .filter(floor -> Objects.equals(floor, currentFloor))
-                .findFirst();
-    }
-
-    private void checkIfGateCanOpen() {
-        Optional<CallLiftData> callLiftData = isCallLiftIsInSameFloorAndDirection(currentFloor, direction);
-        if(callLiftData.isPresent()) {
-            // remove from call list
-            // open & close gate
-            this.callLiftDataList.remove(callLiftData.get());
-            this.openGate();
-            this.goToFloor(5);
-
-        }
-
-        Optional<Integer> goToData = isGoToIsInSameFloor(currentFloor);
-
-        if(goToData.isPresent()) {
-            // remove from go to list
-            // open & close gate
-            this.goToDataList.remove(goToData.get());
-            this.openGate();
-        }
     }
 
     private void openGate() {
@@ -228,24 +121,66 @@ class ElevatorService {
         this.gate = Gate.CLOSE;
     }
 
+    private void emergency() {
+        this.isRunning = false;
+    }
+
+    @Override
+    public void run() {
+        while (this.isRunning) {
+
+            if(this.callLiftDataList.isEmpty()) {
+                // Lift is idle
+                continue;
+            }
+
+            CallLiftData nextMove = elevatorMovementStrategy.findNextMove(this.callLiftDataList, this.currentFloor, this.direction);
+            this.callLiftDataList.remove(nextMove);
+            goToFloor(nextMove.floorCalledFrom);
+            openGate();
+        }
+    }
 }
+
 
 //ELEVATOR CONTROLLER
 class ElevatorController {
-    private List<ElevatorService> elevators;
+    private final ExecutorService executorService;
+    private final List<ElevatorService> elevatorServiceList;
 
-    ElevatorController(List<ElevatorService> elevators) {
-        this.elevators = elevators;
+    ElevatorController(List<ElevatorService> elevatorServiceList) {
+        this.elevatorServiceList = elevatorServiceList;
+        executorService = Executors.newFixedThreadPool(elevatorServiceList.size());
+        elevatorServiceList.forEach(elevatorService -> executorService.submit(elevatorService));
+        executorService.shutdown();
     }
 
-    public void requestElevator(int floor, Direction dir) {
-        // choose best elevator (nearest + same direction OR idle)
-        // forward request
+    public void requestElevator(int elevatorId, int floor, Direction dir) {
+        Optional<ElevatorService> elevatorServiceFromElevatorId = elevatorServiceList
+                .stream()
+                .filter(elevatorService -> elevatorService.getElevatorId() == elevatorId)
+                .findFirst();
+
+        elevatorServiceFromElevatorId.ifPresent(elevatorService -> elevatorService.requestElevator(floor, dir));
+
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ElevatorService elevatorService = new ElevatorService(1, new NearestRequestStrategy());
+        ElevatorService elevatorService1 = new ElevatorService(2, new NearestRequestStrategy());
+
+        List<ElevatorService> elevatorServiceList = new ArrayList<>();
+        elevatorServiceList.add(elevatorService);
+        elevatorServiceList.add(elevatorService1);
+
+        ElevatorController elevatorController = new ElevatorController(elevatorServiceList);
+
+
+        elevatorController.requestElevator(1, 5, Direction.DOWN);
+        elevatorController.requestElevator(1, 2, Direction.UP);
+
     }
 }
